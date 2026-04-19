96ea25d332064bc8411efecef7ac78bb6328b427
NoBodyIsHere-ItsMe <secondarytaleb@gmail.com>
2026-04-19 03:03:29 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Thorfinn',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "ثــورفـيـن",
+  botOwner: 'Moyt', // Your name
+  ownerNumber: '01206178781', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
